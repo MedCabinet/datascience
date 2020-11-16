e8df6c0b7d6148c43d9d346f7a861b429ff1619a
Full Data Alchemist <58147026+Full-Data-Alchemist@users.noreply.github.com>
2020-11-16 16:38:24 -0600

    make app.py file

---
+++ MedCabinet/app.py
@@ -0,0 +1,28 @@
+#DataScience/MedCabinet/app.py
+"""
+#TODO
+"""
+
+#my imports
+import plotly
+import plotly_express
+import dash
+import pandas as pd
+import os
+
+app = dash.Dash(__name__)
+
+TODO = "todo" #erase when all todos are gone
+
+df = TODO
+
+@app.route("/")
+def root():
+    pass
+
+
+class make_app:
+
+    def __init__(slelf):
+        pass
+
