a9e78a662b65fd288d9fb8f464dd94ba95f1a9aa
Full Data Alchemist <58147026+Full-Data-Alchemist@users.noreply.github.com>
2020-11-17 11:45:00 -0600

    add Eli's Model python notebook

---
+++ MedCabinet/__init__.py
@@ -0,0 +1,8 @@
+"""initualizes app"""
+
+
+
+from .app import create_app
+
+
+APP = create_app()
--- MedCabinet/app.py
+++ MedCabinet/app.py
@@ -2,27 +2,40 @@
 """
 #TODO
 """
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
 
-#my imports
+#app imports
 import plotly
-import plotly_express
+import plotly.express as px
+import plotly.graph_objects as go
 import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output, Input
 import pandas as pd
 import os
-
-app = dash.Dash(__name__)
-
-TODO = "todo" #erase when all todos are gone
-
-df = TODO
-
-@app.route("/")
-def root():
-    pass
+import numpy
 
 
-class make_app:
+def create_app():
+    """
+    Creates the app
+    """
+    app = dash.Dash(__name__)
 
-    def __init__(slelf):
-        pass
+    app.layout =html.Div(dcc.Graph(id))
+
+    # DB.init_app(app) #Makes DataBase
+
+    @app.route('/')
+    def root():
+        return 
+
+    # @app.route('/strain')
+    # def strain():
+    #     pass
+
+    return app
+
 
